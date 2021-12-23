fece7a05c6962b2be56ce24e192c39075d432ed3
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-12-23 12:20:05 +0400

    Match test

--- src/basic/match_test.rs
+++ src/basic/match_test.rs
@@ -17,4 +17,11 @@ fn value_in_cents(coin: Coin) -> u8 {
 
 fn main() {
     println!("Value: {}", value_in_cents(Coin::Nickel))
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
 }
