51e3e939ecd5decca68197a35615d6213f42e4c3
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-01-08 18:01:14 +0300

    Simple trait

--- src/basic/generics_test.rs
+++ src/basic/generics_test.rs
@@ -4,12 +4,22 @@ struct Point<T, U> {
     y: U,
 }
 
+pub trait Printable {
+    fn print(&self) -> String;
+}
+
 impl<T, U> Point<T, U> {
     fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
         Point {
             x: self.x,
             y: other.y,
         }
+    }
+}
+
+impl<T, U> Printable for Point<T, U> {
+    fn print(&self) -> String {
+        "ABC".to_string()
     }
 }
 
@@ -21,4 +31,5 @@ fn main() {
     let p3 = p1.mixup(p2);
 
     println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    println!("printed = {}", p3.print());
 }
