32ccfca7a33a44d0d27755b0c788ef80f31020fb
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-01-15 19:45:05 +0300

    Test move

--- src/basic/box_test.rs
+++ src/basic/box_test.rs
@@ -8,4 +8,8 @@ use crate::List::{Cons, Nil};
 
 fn main() {
     let data = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let d = Box::new(5);
+    let x = d;
+    println!("{}", *x);
 }
