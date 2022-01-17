63ff2f9f6d3b8cf8af557a4c8e8d28abbd29b979
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-01-17 18:45:31 +0300

    Split example

--- src/basic/string_test.rs
+++ src/basic/string_test.rs
@@ -3,10 +3,15 @@
 fn main() {
     let hello = "Здравствуйте";
     let answer: &str = &hello[0..4];
-
     for c in hello.chars() {
         println!("{}", c);
     }
-
     println!("The value of x is: {}", answer);
+
+
+    // Split
+
+    let text = "I see the eigenvalue in thine eye";
+    let (head, tail) = text.split_at(21);
+
 }
