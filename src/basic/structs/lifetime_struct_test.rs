8851047b35945a6e95c1f4d9041c60243b2b9f5c
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-03-07 23:41:15 +0300

    two lifetimes

--- src/basic/structs/lifetime_struct_test.rs
+++ src/basic/structs/lifetime_struct_test.rs
@@ -3,9 +3,25 @@ struct S<'a> {
     r: &'a i32
 }
 
+struct S2<'a, 'b> {
+    x: &'a i32,
+    y: &'b i32
+}
+
 fn main() {
     let x = 5;
-    let z = S {
-        r: &x
-    };
+    let r;
+    {
+        let y = 20;
+        {
+            let z = S2 {
+                x: &x,
+                y: &y
+            };
+
+            r = z.x;
+        }
+    }
+
+    println!("Value is: {}", r);
 }
