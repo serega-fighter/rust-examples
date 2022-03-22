4d013a0ddef123d80050e0c556fa7423ffeaad8c
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-03-22 11:27:05 +0300

    &str test

--- src/basic/string_test.rs
+++ src/basic/string_test.rs
@@ -1,16 +1,23 @@
 
 
 fn main() {
-    let hello = "Здравствуйте";
+    let hello = "Здравсвуйте";
     let answer: &str = &hello[0..4];
-    for c in hello.chars() {
+    for c in answer.chars() {
         println!("{}", c);
     }
-    println!("The value of x is: {}", answer);
 
+    // From String
+    let x: String = String::from("ABCCDEF");
+    println!("Just string: {}", x);
+
+    let borrowed: &str = x.as_str();
+    println!("Just borrowed string: {}", borrowed);
+
+    let createdAgain = String::from(borrowed);
+    println!("Just create from borrow: {}", createdAgain);
 
     // Split
-
     let text = "I see the eigenvalue in thine eye";
     let (head, tail) = text.split_at(21);
 
