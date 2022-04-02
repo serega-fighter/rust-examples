bf42241fd9e96e0621a3a79b585324268f5dc879
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-04-02 12:31:04 +0300

    Basic stack

--- src/basic/collections/stack.rs
+++ src/basic/collections/stack.rs
@@ -6,9 +6,26 @@ impl<T> Stack<T> {
     fn new() -> Self {
         Stack { stack: Vec::new() }
     }
+
+    fn size(&self) -> usize {
+        self.stack.len()
+    }
+
+    fn push(&mut self, item: T) {
+        self.stack.push(item)
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
 }
 
 fn main() {
-    let s = Stack {stack: Vec::new() };
+    let st = Stack {stack: Vec::new() };
 
+    st.push(String::from("ABC"));
+    st.push(String::from("DEF"));
+    st.push(String::from("CDA"));
+
+    println!("Stack has {} elements", st.size());
 }
