468bb33bbe59116d76faa1c6ec80c1644cac392f
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-04-03 12:25:45 +0400

    Vector methods

--- src/basic/collections/vector_test.rs
+++ src/basic/collections/vector_test.rs
@@ -5,10 +5,17 @@ fn main() {
     v.push(String::from("A"));
     v.push(String::from("B"));
     v.push(String::from("C"));
-    v.push(String::from("D"));
 
+    // Iterate with index
     for (pos, e) in v.iter().enumerate() {
         println!("Element at position {}: {:?}", pos, e);
+    }
+
+    let sz: usize = v.len();
+    println!("Size is {}", sz);
+
+    for i in 0..sz {
+        println!("Element is {}", i);
     }
 
     for i in &v {
