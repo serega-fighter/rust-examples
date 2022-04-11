9473909930b19acc2dcc8c42182b2626f73df25b
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-04-11 19:05:43 +0300

    Ref test

--- src/basic/refs/ref_test.rs
+++ src/basic/refs/ref_test.rs
@@ -1,6 +1,23 @@
+use std::collections::HashMap;
+
+type Table = HashMap<String, Vec<String>>;
 
 pub fn change_str(s: &mut String) {
     s.push_str("AAC");
+}
+
+pub fn showTable(table: &mut Table) {
+    for (artist, works) in table {
+
+        let ar: &String = artist;
+        println!("works by {}:", ar);
+
+        let w: &Vec<String> = works; // casted to ref
+
+        for work in w {
+            println!(" {}", work);
+        }
+    }
 }
 
 fn main() {
@@ -8,4 +25,11 @@ fn main() {
 
     change_str(&mut s);
     println!("The value of x is: {}", s);
+
+    let mut table = Table::new();
+    table.insert("Gesualdo".to_string(), vec!["many madrigals".to_string(), "Tenebrae Responsoria".to_string()]);
+    table.insert("Caravaggio".to_string(), vec!["The Musicians".to_string(), "The Calling of St. Matthew".to_string()]);
+    table.insert("Cellini".to_string(), vec!["Perseus with the head of Medusa".to_string(), "a salt cellar".to_string()]);
+
+    showTable(&mut table);
 }
