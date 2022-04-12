93eb8a7009fcb979651643f3e047356972c4a7fb
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-04-12 10:15:43 +0300

    Range test

--- src/basic/utility/range_test.rs
+++ src/basic/utility/range_test.rs
@@ -2,8 +2,12 @@ use std::ops::Range;
 use std::ops::RangeFrom;
 
 pub fn main() {
-    let a: Range<i32> = 1..100;
+    let a: Range<i32> = 1..5;
     println!("{:?}", a);
+
+    for x in a {
+        println!("Next value is {}", x);
+    }
 
     let b: RangeFrom<i32> = 1..;
     println!("{:?}", b);
