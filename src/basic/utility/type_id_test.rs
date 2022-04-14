4e706a425abb406b41ab33fdfed17899649a7c9e
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-04-14 09:31:43 +0300

    Type id test

--- src/basic/utility/type_id_test.rs
+++ src/basic/utility/type_id_test.rs
@@ -6,4 +6,14 @@ fn is_string(s: &dyn Any) -> bool {
 pub fn main() {
     assert_eq!(is_string(&0), false);
     assert_eq!(is_string(&"cookie monster".to_string()), true);
+
+    let boxed: Box<dyn Any> = Box::new(3_i32);
+
+// You're more likely to want this:
+    let actual_id = (&*boxed).type_id();
+// ... than this:
+    let boxed_id = boxed.type_id();
+
+    assert_eq!(actual_id, TypeId::of::<i32>());
+    assert_eq!(boxed_id, TypeId::of::<Box<dyn Any>>());
 }
