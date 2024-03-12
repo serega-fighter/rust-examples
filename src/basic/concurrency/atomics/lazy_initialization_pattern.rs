8125fd8a756e8a09e2b1d35bc03cb4fcf8507819
Serega <26937163+serega-fighter@users.noreply.github.com>
2024-03-12 21:46:12 +0300

    lazy init with pointer to data

--- src/basic/concurrency/atomics/lazy_initialization_pattern.rs
+++ src/basic/concurrency/atomics/lazy_initialization_pattern.rs
@@ -1,28 +1,41 @@
-use std::sync::atomic::AtomicU64;
-use std::sync::atomic::Ordering::Relaxed;
+use std::sync::atomic::AtomicPtr;
+use std::sync::atomic::Ordering::{Acquire, Release};
 use std::thread;
 
-fn calculate_x() -> u64 {
-    2 * 5
+#[derive(Debug)]
+struct Data {
+    name: String,
+    age: u32,
 }
 
-fn get_x() -> u64 {
-    static X: AtomicU64 = AtomicU64::new(0);
-    let mut x = X.load(Relaxed);
-    if x == 0 {
-        x = calculate_x();
-        X.store(x, Relaxed);
+fn generate_data() -> Data {
+    return Data {name: String::from("Serega"), age: 29}
+}
+
+fn get_data() -> &'static Data {
+
+    static PTR: AtomicPtr<Data> = AtomicPtr::new(std::ptr::null_mut());
+
+    let mut p = PTR.load(Acquire);
+
+    if p.is_null() {
+        p = Box::into_raw(Box::new(generate_data()));
+        if let Err(e) = PTR.compare_exchange(std::ptr::null_mut(), p, Release, Acquire) {
+            drop(unsafe { Box::from_raw(p) }); // drop explicitly
+            p = e;
+        }
     }
-    x
+
+    unsafe { &*p }
 }
 
 pub fn main() {
     thread::scope(|scope| {
         scope.spawn(|| {
-            println!("Calculated: {}", get_x());
+            println!("Calculated: {:?}", generate_data());
         });
         scope.spawn(|| {
-            println!("Calculated: {}", get_x());
+            println!("Calculated: {:?}", generate_data());
         });
     });
 }
